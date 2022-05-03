a8bc222eaaa931c27fde5bdced3fde759423fc1b
shinzoku <shinzoku62800@gmail.com>
2022-05-03 15:19:03 +0200

    Mises à jour des exercices sur le SQL
    Exercices terminés

--- exercice-04.sql
+++ exercice-04.sql
@@ -2,11 +2,11 @@
 -- Requêtes avec jointure « many to one » et « one to many »
 
 -- Exo 4.1
--- Listez tous les students avec leurs projets
+-- Listez tous les students avec leurs projects
 SELECT * FROM `student` INNER JOIN `project` ON student.project_id = project.id;
 
 -- Exo 4.2
--- Listez le student dont l'id est `2` avec son projet
+-- Listez le student dont l'id est `2` avec son project
 SELECT * FROM `student` INNER JOIN `project` ON student.project_id = project.id WHERE student.id = 2;
 
 -- Exo 4.3
